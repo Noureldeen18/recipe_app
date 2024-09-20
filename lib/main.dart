bc25d7d15c707bee834c0f9b1551bde06527d983
Nour Eldeen Hany <127895311+Noureldeen18@users.noreply.github.com>
2024-09-20 18:39:29 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart'; // Import Firebase core
+import 'package:firebase_core/firebase_core.dart';
+import 'package:recipe_app/recipe_menu_page.dart';
+import 'package:recipe_app/recipe_view_page.dart';
 import 'login_page.dart';
 import 'register_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();  // Ensure Flutter is initialized before Firebase
-  await Firebase.initializeApp();             // Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -13,16 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Recipe App',
-      theme: ThemeData(
-        primaryColor: Color(0xFFf96163),
-        fontFamily: 'Poppins',
-      ),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => LoginPage(),
-        '/register': (context) => RegisterPage(),
-      },
+      debugShowCheckedModeBanner: false,
+     home: MealDetailScreen(idMeal: ""),
     );
   }
 }
