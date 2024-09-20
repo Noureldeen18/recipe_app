b34bfd4cf5cea4efaf7ebbc825aafb62a2ff96b0
bor3yy <121395134+bor3yy@users.noreply.github.com>
2024-09-21 00:37:16 +0300

    Added favourite iconButton to the menu page

--- lib/recipe_menu_page.dart
+++ lib/recipe_menu_page.dart
@@ -11,8 +11,9 @@ class Test extends StatefulWidget {
 
 class _MenuViewState extends State<Test> {
   List<String> categories = [];
-  List<Map<String, String>> meals = [];  // Store meals for the selected category
+  List<Map<String, String>> meals = []; // Store meals for the selected category
   int _selectedIndex = 0;
+  Set<String> favoriteMeals = Set<String>();
 
   @override
   void initState() {
@@ -24,8 +25,7 @@ class _MenuViewState extends State<Test> {
   void fetchCategories() {
     DioHelper.getData(url: 'api/json/v1/1/categories.php').then((value) {
       setState(() {
-        categories = List<String>.from(
-            value?.data['categories'].map((category) => category['strCategory']));
+        categories = List<String>.from(value?.data['categories'].map((category) => category['strCategory']));
       });
       // After fetching categories, fetch meals for the default (first) category
       fetchMeals(categories[0]);
@@ -42,11 +42,11 @@ class _MenuViewState extends State<Test> {
     ).then((value) {
       setState(() {
         meals = List<Map<String, String>>.from(
-            (value?.data['meals'] as List<dynamic>).map((meal) => {
-              'idMeal': meal['idMeal'] as String,
-              'strMeal': meal['strMeal'] as String,
-              'strMealThumb': meal['strMealThumb'] as String,
-            })
+          (value?.data['meals'] as List<dynamic>).map((meal) => {
+            'idMeal': meal['idMeal'] as String,
+            'strMeal': meal['strMeal'] as String,
+            'strMealThumb': meal['strMealThumb'] as String,
+          }),
         );
       });
     }).catchError((error) {
@@ -60,7 +60,9 @@ class _MenuViewState extends State<Test> {
       appBar: AppBar(
         backgroundColor: Color(0xfffdcbcb),
         leading: IconButton(
-          onPressed: () {},
+          onPressed: () {
+
+          },
           icon: Icon(Icons.menu),
           color: Colors.black,
         ),
@@ -109,12 +111,10 @@ class _MenuViewState extends State<Test> {
                       setState(() {
                         _selectedIndex = index;
                       });
-                      fetchMeals(categories[index]);  // Fetch meals when a category is selected
+                      fetchMeals(categories[index]); // Fetch meals when a category is selected
                     },
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: _selectedIndex == index
-                          ? Color(0xFFf96163)
-                          : Colors.white,
+                      backgroundColor: _selectedIndex == index ? Color(0xFFf96163) : Colors.white,
                     ),
                     child: Text(categories[index]),
                   ),
@@ -128,44 +128,94 @@ class _MenuViewState extends State<Test> {
             child: GridView.builder(
               padding: EdgeInsets.all(10.0),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,  // 2 items per row
+                crossAxisCount: 2, // 2 items per row
                 crossAxisSpacing: 5.0,
                 mainAxisSpacing: 5.0,
               ),
               itemCount: meals.length,
-              itemBuilder:(context, index) {
+              itemBuilder: (context, index) {
+                final meal = meals[index];
+                final isFavorite = favoriteMeals.contains(meal['idMeal']!);
+
                 return GestureDetector(
-                    onTap: () {
-                      // Navigate to MealDetailScreen when a meal is clicked
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MealDetailScreen(
-                            idMeal: meals[index]['idMeal']!,
+                  onTap: () {
+                    // Navigate to MealDetailScreen when a meal is clicked
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MealDetailScreen(
+                          idMeal: meal['idMeal']!,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    elevation: 3.0,
+                    child: Stack(
+                      children: [
+                        // Image
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(
+                              child: Image.network(
+                                meal['strMealThumb']!,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Text(
+                                meal['strMeal']!,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                          ],
+                        ),
+                        // Favorite icon in the top-right corner of the image
+                        Positioned(
+                          top: 8,
+                          right: 8,
+                          child: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                if (isFavorite) {
+                                  favoriteMeals.remove(meal['idMeal']);
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text( "Removed from Favorites"),
+                                      duration: Duration(seconds: 2),
+                                      backgroundColor: Colors.black,
+                                    ),
+                                  );
+
+                                } else {
+                                  favoriteMeals.add(meal['idMeal']!);
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text( "Added To Favorites"),
+                                      duration: Duration(seconds: 2),
+                                      backgroundColor: Colors.black,
+                                    ),
+                                  );
+                                }
+                              });
+                            },
+                            icon: Icon(
+                              Icons.favorite,
+                              color: isFavorite ? Colors.pink : Colors.grey,
+                            ),
                           ),
                         ),
-                      );
-                    },
-                    child: Card(
-                      elevation: 3.0,
-                      child: Column(
-                        children: [
-                          Image.network(
-                            meals[index]['strMealThumb']!,
-                            height: 100,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            meals[index]['strMeal']!,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 10),
-                          ),
-                        ],
-                      ),
-                    )
+                      ],
+                    ),
+                  ),
                 );
               },
             ),
